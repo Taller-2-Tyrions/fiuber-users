12dc7026e7d6b0b7b3e32ae2b700395a05ad678e
mateocapon <mcapon@fi.uba.ar>
2022-09-07 14:18:25 -0300

    probando con actions

---
+++ tests/registry.py
@@ -0,0 +1,12 @@
+import unittest
+
+class RegistryTest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(1,1)
+
+    def test2(self):
+        self.assertEqual(1,2)
+
+
+if __name__ == '__main__':
+        unittest.main()
