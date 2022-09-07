7f9a1882d628f0d411e78270c1ec07a223f2249e
mateocapon <mcapon@fi.uba.ar>
2022-09-07 14:32:04 -0300

    proabndo actions

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
-
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,11 +1,12 @@
 import unittest
 
+
 class RegistryTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def test2(self):
-        self.assertEqual(1,2)
+        self.assertEqual(1, 2)
 
 
 if __name__ == '__main__':
