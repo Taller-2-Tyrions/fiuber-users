f518538468ef729cf9b17611f53dfd64aa0720b4
gsabatino <gsabatino@fi.uba.ar>
2022-09-08 11:55:32 -0300

    foo

--- app/main.py
+++ app/main.py
@@ -17,9 +17,11 @@ app.add_middleware(
     allow_headers=["*"],
     )
 
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT')))
