5ce92896393acf723c69379b4b055a8f0ed2eb9a
SaFernandezC <SFernandezC@fi.uba.ar>
2022-09-13 15:48:42 -0300

    Agregamos CRUD para users
    Co-authored-by: niragui <niragui@users.noreply.github.com>

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,27 @@
 from fastapi import APIRouter
-from ..schemas.usersSchema import UserBase
-from ..crud import users
+from ..schemas.usersSchema import UserBase, DriverBase
+from ..crud import crud
 from ..database.mongo import db
+from typing import Union
 
 router = APIRouter(
     prefix="/users",
     tags=['Users']
 )
 
-
 @router.post('/find_user')
-def find_user(user: UserBase):
-    return users.find_user(db, user.name)
+def find_user(user_id: str):
+    return crud.find_user(db, user_id)
+
+@router.post('/create_user')
+def create_user(user: Union[UserBase, DriverBase]):
+    return crud.create_user(db, user)
+    
+
+@router.post('/update_user')
+def update_user(user_id: str, changes:dict):
+    return crud.update_user(db, user_id, changes)
+
+@router.post('/delete_user')
+def delete_user(user_id: str):
+    return crud.delete_user(db, user_id)
--- app/schemas/usersSchema.py
+++ app/schemas/usersSchema.py
@@ -1,17 +1,13 @@
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
+class PersonBase(BaseModel):
+    id: str
     name: str
     last_name: str
 
+class UserBase(PersonBase):
+    address: str
 
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-
-    class Config:
-        orm_mode = True
+class DriverBase(PersonBase):
+    car: str
