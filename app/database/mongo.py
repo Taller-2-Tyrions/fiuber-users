4b4c8b03afb5ebcf741551e0869bf74e5493f187
Gonzalo Sabatino <72721443+gsabatino9@users.noreply.github.com>
2022-09-15 16:30:49 -0300

    Update mongo.py

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient("mongodb+srv://fiuber_admin:gpsvj06RJ1QZVV4t@fiuber.qn2hoxw.mongodb.net/?retryWrites=true&w=majority", 8000)
+aux = "mongodb+srv://fiuber_admin:gpsvj06RJ1QZVV4t@fiuber.qn2hoxw.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(aux, 8000)
 db = client[clients]
