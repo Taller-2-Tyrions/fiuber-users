6e07173c9d6ec3b291c8aebe22e84a76767705b0
gsabatino <gsabatino@fi.uba.ar>
2022-09-15 16:33:32 -0300

    cambios

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-aux = "mongodb+srv://fiuber_admin:gpsvj06RJ1QZVV4t@fiuber.qn2hoxw.mongodb.net/?retryWrites=true&w=majority"
+aux = "mongodb+srv://fiuber_admin:gpsvj06RJ1QZVV4t@fiuber."
+aux += "qn2hoxw.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(aux, 8000)
-db = client[clients]
+db = client["clients"]
